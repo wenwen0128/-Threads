321c13a098ed01a2bd64e53b7d94e3aca9729f77
wen <bravoj05505@gmail.com>
2023-07-31 22:18:28 +0800

    Initial Commit

---
+++ 仿照一頁app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  仿照一頁app
+//
+//  Created by 溫皓 on 2023/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let searchController = UISearchController()
+           navigationItem.searchController = searchController
+    }
+
+
+}
+
+
